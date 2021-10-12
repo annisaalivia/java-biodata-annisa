710ed34617e5dbe88f32ae03db741c1efcdd0af3
annisaalivia <annisaaliviia@gmail.com>
2021-10-12 12:20:12 +0700

    Challenge 1

---
+++ BiodataDiri.java
@@ -0,0 +1,23 @@
+public class BiodataDiri{
+    public static void main(String[] args) {
+        //firstname, lastname, address, umur, hobby, tinggi, makananKesukaan, phoneNumber
+        String firstname = " Annisa" ;
+        String lastname = " Alivia" ;
+        String address = " Medan";
+        int umur = 19;
+        String hobby = " Makan" ;
+        double tinggi =  158.5 ;
+        String makananKesukaan = " Siomay, bakso, ayam goreng" ;
+        String phoneNumber = " 1234577" ;
+
+
+        System.out.println("firstname :" + firstname);
+        System.out.println("lastname :" + lastname);
+        System.out.println("address :" + address);
+        System.out.println("umur :" + 19 + " tahun");
+        System.out.println("hobby :" + hobby);
+        System.out.println("tinggi :" + tinggi + " cm");
+        System.out.println("makanan kesukaan :" + makananKesukaan);
+        System.out.println("phone number :" + phoneNumber);
+    }
+}
